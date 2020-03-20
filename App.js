81084e6174bc86e5e2028499a705e970eebc184c
Rup <erdem.gonul@ozu.edu.tr>
2020-03-20 15:25:07 +0300

    reduced unused files

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator ,TransitionPresets} from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
-import BetScreen from './src/screens/BetScreen';
-import AuthScreen from './src/screens/AuthScreen';
-import SignupScreen from './src/screens/SignupScreen';
-import SignInScreen from './src/screens/SignInScreen';
 import ChangeLocation from './src/screens/ChangeLocation';
 import RestaurantScreen from './src/screens/RestaurantScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    BetScreen: BetScreen,
-    AuthScreen: AuthScreen,
-    SignupScreen: SignupScreen,
-    SignInScreen: SignInScreen,
     ChangeLocation : ChangeLocation,
     RestaurantScreen:RestaurantScreen
   },
